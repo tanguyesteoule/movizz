82dfccb1f7a417fe3b6f4491e939fa29bf169469
Tanguy <tanguy.esteoule@meteoswift.com>
2022-07-19 19:42:54 +0200

    update popularity

---
+++ guess_movie/quizz/management/commands/update_popularity.py
@@ -0,0 +1,23 @@
+import pickle
+from django.core.management import BaseCommand
+from quizz.models import Screenshot, Movie
+from pathlib import Path
+import os
+import pandas as pd
+
+DATA_DIR = Path(__file__).resolve().parent.parent / 'data'
+
+
+class Command(BaseCommand):
+    help = "Doublon"
+
+    def handle(self, *args, **kwargs):
+        path_file = os.path.join(DATA_DIR, 'dict_popularity.p')
+        dict_popularity = pickle.load(open(path_file, "rb"))
+
+        for tt in dict_popularity.keys():
+            popularity = dict_popularity[tt]
+            print(tt, popularity)
+            m = Movie.objects.get(imdb_id=tt)
+            m.popularity = popularity
+            m.save()
