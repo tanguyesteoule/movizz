9ba566abbcd9affb8e24798e829f18b465e42d11
Tanguy <tanguy.esteoule@meteoswift.com>
2022-07-21 19:19:16 +0200

    update nsfw iterate only if has_image is false

--- guess_movie/quizz/management/commands/update_nsfw.py
+++ guess_movie/quizz/management/commands/update_nsfw.py
@@ -14,7 +14,11 @@ class Command(BaseCommand):
         path_file = os.path.join(DATA_DIR, 'nsfw.p')
         dict_nsfw = pickle.load(open(path_file, "rb"))
 
-        for movie_id, img_ids in dict_nsfw.items():
+        list_movie = Movie.objects.filter(imdb_id__in=list(dict_nsfw.keys()), has_image=False)
+
+        for movie in list_movie:
+            movie_id = movie.imdb_id
+            img_ids = dict_nsfw[movie_id]
             if img_ids is None:
                 continue
             for img_id in img_ids:
