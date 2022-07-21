960666bf6e7df3ab2709de2cb5c23126f5a11877
Tanguy <tanguy.esteoule@meteoswift.com>
2022-07-21 20:06:30 +0200

    fix bug script nsfw

--- guess_movie/quizz/management/commands/update_nsfw.py
+++ guess_movie/quizz/management/commands/update_nsfw.py
@@ -19,20 +19,20 @@ class Command(BaseCommand):
         for movie in list_movie:
             movie_id = movie.imdb_id
             img_ids = dict_nsfw[movie_id]
-            if img_ids is None:
-                continue
-            for img_id in img_ids:
-                try:
-                    print(f'{movie_id}/{img_id}.jpg')
-                    screenshot = Screenshot.objects.get(image=f'{movie_id}/{img_id}.jpg')
-                    screenshot.sfw = 0
-                    screenshot.save()
-                except:
-                    print(f'{movie_id}/{img_id}.jpg', ' - ERROR when inserting')
+            if img_ids is not None:
+                for img_id in img_ids:
+                    try:
+                        print(f'{movie_id}/{img_id}.jpg')
+                        screenshot = Screenshot.objects.get(image=f'{movie_id}/{img_id}.jpg')
+                        screenshot.sfw = 0
+                        screenshot.save()
+                    except:
+                        print(f'{movie_id}/{img_id}.jpg', ' - ERROR when inserting')
 
             try:
                 movie = Movie.objects.get(imdb_id=movie_id)
                 movie.has_image = True
                 movie.save()
+                print(f'{movie_id} - Change has_image to True')
             except:
                 print(f'{movie_id} - ERROR when inserting (Does not exist)')
